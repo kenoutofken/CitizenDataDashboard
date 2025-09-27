bff2f185aa3e2ddd88938f91f15dd6029395e2cf
Ken <yauken@outlook.com>
2025-09-27 02:34:29 -0700

    Added Hero Icons, Framer Motion, and added layer base CSS for h1, h2, and p elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import "./App.css";
+import { motion } from "framer-motion";
+import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+
+const myAnimation = {
+  initial: { opacity: 0, y: 50 },
+  inView: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+};
 
 function App() {
   return (
     <>
       <div class="hero bg-base-200 min-h-screen">
         <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Hello there</h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button class="btn btn-primary">Get Started</button>
-          </div>
+          <motion.div
+            initial="initial"
+            whileInView="inView"
+            viewport={{ once: true, amount: 0.7 }}
+            variants={myAnimation}
+          >
+            <div class="max-w-md">
+              <video
+                src="/videos/hello.webm"
+                autoPlay
+                loop
+                muted
+                className="w-40 mx-auto"
+              />
+              <h1>Hello there</h1>
+              <p className="py-6">
+                This is a boilerplate for React + Vite + TailwindCSS + DaisyUI.
+              </p>
+              <button class="btn btn-primary">
+                Get Started{" "}
+                <HandThumbUpIcon className="inline-block align-middle w-[1em] h-[1em]" />
+              </button>
+            </div>
+          </motion.div>
         </div>
       </div>
     </>
